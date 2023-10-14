6dbe033f1ce37ff88917472f544c4d188c22610c
Bassel-saleh <basselh26@gmail.com>
2023-10-14 07:23:41 -0700

    added struct

--- main.h
+++ main.h
@@ -3,4 +3,13 @@
 
 int _printf(const char *format, ...);
 
+/**
+ * struct datatype - struct datatype
+ * @f: the function to be used
+ */
+typedef struct datatype
+{
+	void (*f)(va_list);
+} datatype;
+
 #endif
