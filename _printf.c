f13204af7d145950d479784581dfeca32274157e
Bassel-saleh <basselh26@gmail.com>
2023-10-14 16:58:30 -0700

    making some progress

--- _printf.c
+++ _printf.c
@@ -2,62 +2,85 @@
 #include <stdarg.h>
 
 /**
- */
-void print_dec(va_list var)
-{
-	int num;
-
-	num = va_arg(var, int);
-	print_number(num);
-}
-
-/**
+ * _printf - Custom printf function
+ * @format: The format string
+ * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int i, count;
 	va_list var;
+	int count = 0;
 
+	if (format)
+	{
+		va_start(var, format);
+		count = _print_format(format, var);
+		va_end(var);
+	}
+
+	return count;
+}
+
+/**
+ * _handle_percent - Handle the '%' character
+ * @format: The format string
+ * @var: The va_list to access arguments
+ * @count: Pointer to the character count
+ */
+void _handle_percent(const char **format, va_list var, int *count)
+{
+	(*format)++; /* Move past '%' */
+	if (**format == '%')
+		*count += _putchar('%');
+	else
+		*count += _process_format(format, var);
+}
+
+/**
+ * _process_format - Process format specifiers
+ * @format: The format string
+ * @var: The va_list to access arguments
+ * Return: Number of characters printed
+ */
+int _process_format(const char **format, va_list var)
+{
+	int i, count = 0;
 	datatype type[] = {
 		{'c', print_char},
 		{'s', print_str},
 		{'d', print_dec},
-		{'\0', 0}
+		{'\0', NULL}
 	};
-	va_start(var, format);
-	count = 0;
+	i = 0;
+	while (type[i].choice)
+	{
+		if (type[i].choice == **format)
+		{
+			count += type[i].f(var);
+			break;
+		}
+		i++;
+	}
+	return count;
+}
+
+/**
+ * _print_format - Handle the format string and print characters
+ * @format: The format string
+ * @var: The va_list to access arguments
+ * Return: Number of characters printed
+ */
+int _print_format(const char *format, va_list var)
+{
+	int count = 0;
+
 	while (*format)
 	{
 		if (*format == '%')
-		{
-			format++;
-			if (*format == '%')
-			{
-				_putchar('%');
-				count++;
-			}
-			else
-			{
-				i = 0;
-				while (type[i].choice)
-				{
-					if (type[i].choice == *format)
-					{
-						type[i].f(var);
-						count++;
-						break;
-					}
-					i++;
-				}
-			}
-		}
+			_handle_percent(&format, var, &count);
 		else
-		{
-			_putchar(*format);
-			count++;
-		}
-	format++;
+			count += _putchar(*format);
+		format++;
 	}
-	va_end(var);
 	return (count);
 }
