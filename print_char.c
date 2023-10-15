c98ce834b49290ad2bf5f87c59623e76495324d6
Bassel-saleh <basselh26@gmail.com>
2023-10-15 11:57:29 -0700

    some modifications

--- print_char.c
+++ print_char.c
@@ -1,11 +1,16 @@
 #include "main.h"
 #include <stdarg.h>
-
+/**
+ * print_char - prints char
+ * @var: specifier list
+ * Return: i which is 1 if a char was printed
+ */
 int print_char(va_list var)
 {
 	int c;
 
 	c = va_arg(var, int);
-	_putchar(c);
+	if (c != 0)
+		_putchar(c);
 	return (1);
 }
